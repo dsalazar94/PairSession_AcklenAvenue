9f5bfc330850e80f97b29f2f4f2dbccf1472d641
dsalazar94 <salazarm.daniel@outlook.com>
2020-09-25 17:30:23 -0400

    Pong

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
     {
-        public int Add()
-        {
-            return -1;
+        //private int number_1;
+        public int Add(string number = "0")
+        { 
+            return Int32.Parse(number);
         }
     }
 }
